f8bbf61dc40ddaf26d7cb5ee2d457a5bf866070a
Stephan Moerman <stephan.moerman@viewnary.com>
2024-01-03 11:58:12 +0100

    Add new password functionality to the authentication system

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetPasswordLink = `http://localhost:3000/auth/update-password?token=${token}`;
+  const resetPasswordLink = `http://localhost:3000/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@bakklog.com",
